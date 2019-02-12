d1c3baaf4f37a9dfc670c7a240758ae70b67c8c2
sunfen <sunfen@git>
2019-02-12 21:10:09 +0800

    update

--- miniprogram/pages/component/footernav/footernav.js
+++ miniprogram/pages/component/footernav/footernav.js
@@ -2,19 +2,20 @@ const app = getApp()
 Component({
   properties: {
     // 这里定义了innerText属性，属性值可以在组件使用时指定
+    current:{
+      type: String,
+      value: 0
+    },
 
   },
   data: {
     // 这里是一些组件内部数据
   },
   methods: {
-    gotoCompanyIndex: function () {
+    gotoHome: function () {
       wx.navigateTo({
         url: '/pages/index/index',
       })
-    },
-    gotoMessages: function () {
-      console.log("圈子");
     },
     bindViewMy: function () {
       wx.navigateTo({
---
+++ miniprogram/pages/user/user.js
@@ -0,0 +1,44 @@
+//index.js
+const app = getApp()
+
+
+var common = require('/../../pages/common/common.js');
+
+
+var header = app.globalData.header;
+
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //用户个人信息
+    userInfo: {
+      avatarUrl: "",//用户头像
+      nickName: "",//用户昵称
+    }
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    /**
+     * 获取用户信息
+     */
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        var avatarUrl = 'userInfo.avatarUrl';
+        var nickName = 'userInfo.nickName';
+        that.setData({
+          [avatarUrl]: res.userInfo.avatarUrl,
+          [nickName]: res.userInfo.nickName,
+        })
+      }
+    })
+  }
+})
+
