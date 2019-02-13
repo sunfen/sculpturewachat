cace43a8865df80f16f43abcab009eaa7fcdc58e
sunfen <sunfen@git>
2019-02-13 21:04:23 +0800

    update

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -14,40 +14,8 @@ Page({
     isLogin: false,
     results: [],
     pageInfo:{size: 10, page:0},
-    navData: [
-      {
-        name: "今日求购",  //文本
-        current: 1,    //是否是当前页，0不是  1是
-        style: 0,     //样式
-        ico: 'icon-qiugouguanli',  //不同图标
-        fn: 'gotoCompanyIndex'   //对应处理函数
-      }, {
-        name: "名片",
-        current: 0,
-        style: 0,
-        ico: 'icon-mingpianjia',
-        fn: 'gotobusinessCard'
-      }, {
-        name: "发布",
-        current: 0,
-        style: 1,
-        ico: '',
-        fn: 'gotopublish'
-      }, {
-        name: "消息",
-        current: 0,
-        style: 0,
-        ico: 'icon-yikeappshouyetubiao35',
-        fn: 'gotoMessages',
-        msg: 2   //因为消息是这个“消息”特有的，所以只有这个对象下游msg键值
-      }, {
-        name: "我的",
-        current: 0,
-        style: 0,
-        ico: 'icon-wode',
-        fn: 'bindViewMy'
-      },
-    ],
+    howModal: false,
+    showalert: false
   },
   getpage() {
     var that = this;
@@ -129,6 +97,33 @@ Page({
     })
   },
 
+
+  go: function () {
+
+    this.setData({
+
+      showModal: false
+
+    })
+
+  },
+  // 弹出项目详情页
+  viewdetail(){
+    this.setData({
+      showModal: true
+    })
+  },
+  alert: function () {
+
+    var that = this;
+
+    this.setData({
+
+      showalert: !that.data.showalert
+
+    })
+
+  },
   //跳转新增项目页面
   addProject() {
     wx.navigateTo({
