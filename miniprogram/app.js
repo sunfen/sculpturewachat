e33c702b7754894c37c940d770e5f9b9392c1715
sunfen <sunfen@git>
2019-03-07 15:17:22 +0800

    update

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,8 +4,8 @@ App({
     var that = this;
     that.globalData = {
       //urlPath: "http://39.106.157.46:8080/",
-      //urlPath:"https://www.art-sculpture.cn/",
-      urlPath: "http://localhost:8081/",
+      urlPath:"https://www.art-sculpture.cn/",
+     // urlPath: "http://localhost:8081/",
       //urlPath: "http://192.168.1.105:8081/",
       openid:'',
       userInfo: {},
