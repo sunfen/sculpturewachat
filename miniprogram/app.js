4df00716a6142ebf76ba8381e54d7013f5d7540a
sunfen <sunfen@git>
2019-03-14 17:59:07 +0800

    update

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,8 +4,8 @@ App({
     var that = this;
     that.globalData = {
       //urlPath: "http://39.106.157.46:8080/",
-      //urlPath:"https://www.art-sculpture.cn/",
-     urlPath: "http://localhost:8081/",
+      urlPath:"https://www.art-sculpture.cn/",
+     //urlPath: "http://localhost:8081/",
       //urlPath: "http://192.168.1.105:8081/",
       openid:'',
       userInfo: {},
@@ -33,6 +33,34 @@ App({
     wx.showShareMenu({
       withShareTicket: true
     })
-  },
 
+
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate(function (res) {
+        // 请求完新版本信息的回调
+        console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success(res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate()
+            }
+          }
+        }) 
+    })
+
+    updateManager.onUpdateFailed(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本下载失败',
+        showCancel: false
+      })
+    })
+  },
 })
